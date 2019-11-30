3d31d8ae24e84a70e86a25b0f1084bd5d8183401
Karl Keefer <karl@karlkeefer.com>
2019-11-29 23:24:44 -0800

    add tests for RequireAuth in jwt

--- golang/server/jwt/jwt_test.go
+++ golang/server/jwt/jwt_test.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,27 +15,71 @@ import (
 // expiredToken has uid of 30
 const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyIjp7ImlkIjozMCwibmFtZSI6IiIsInN0YXR1cyI6MX0sImV4cCI6MTU3NTA4Njg5MCwiaWF0IjoxNTc1MDg2ODgzfQ.8HA0d-yOQNl6JIuQUfnuZFVxOgb6799KgmEGcugqFy0"
 
-// Note: more thorough testing of encode/decode stuff requires us to mock out the
-// time package, which would be a big PITA
-func TestDecodeUser(t *testing.T) {
-	token := encodeUser(&user.User{
-		ID:     30,
-		Status: user.StatusActive,
+func TestRequireAuth(t *testing.T) {
+	// setup mock
+	ur := user.Mock(nil, nil)
+	env := env.Mock(nil, ur, nil)
+
+	t.Run("no token at all", func(t *testing.T) {
+		r := httptest.NewRequest(http.MethodGet, "/path", nil)
+		w := httptest.NewRecorder()
+
+		RequireAuth(user.StatusActive, env, w, r, func(u *user.User) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {}
+		})(w, r)
+
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+		assert.Equal(t, "{\"Error\":\"You don't have permission to view this resource\"}\n", string(body))
 	})
-	assert.NotEmpty(t, token)
 
-	du, err := decodeUser(token)
-	assert.NoError(t, err)
-	assert.Equal(t, int64(30), du.ID)
-	assert.Equal(t, user.Status(1), du.Status)
+	t.Run("with a valid token", func(t *testing.T) {
+		r := httptest.NewRequest(http.MethodGet, "/path", nil)
+		w := httptest.NewRecorder()
+		r.AddCookie(&http.Cookie{
+			Name: cookieName,
+			Value: encodeUser(&user.User{
+				ID:     30,
+				Status: user.StatusActive,
+			}),
+		})
 
-	// test old token
-	_, err = decodeUser(expiredToken)
-	assert.Equal(t, errors.ExpiredToken, err)
+		RequireAuth(user.StatusActive, env, w, r, func(u *user.User) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, int64(30), u.ID)
+				w.Write([]byte("hi"))
+			}
+		})(w, r)
 
-	// test garbage token
-	_, err = decodeUser("garbage_token")
-	assert.Equal(t, errors.InvalidToken, err)
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+		// make sure our handler fired
+		assert.Equal(t, "hi", string(body))
+	})
+
+	t.Run("with a valid token, but not enough permissions", func(t *testing.T) {
+		r := httptest.NewRequest(http.MethodGet, "/path", nil)
+		w := httptest.NewRecorder()
+		r.AddCookie(&http.Cookie{
+			Name: cookieName,
+			Value: encodeUser(&user.User{
+				ID:     30,
+				Status: user.StatusActive,
+			}),
+		})
+
+		RequireAuth(user.StatusAdmin, env, w, r, func(u *user.User) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, int64(30), u.ID)
+				w.Write([]byte("hi"))
+			}
+		})(w, r)
+
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+		// make sure our handler fired
+		assert.Equal(t, "{\"Error\":\"You don't have permission to view this resource\"}\n", string(body))
+	})
 }
 
 func TestHandleUserCookie(t *testing.T) {
@@ -136,4 +181,27 @@ func TestUserFromCookie(t *testing.T) {
 	u, err = userFromCookie(r)
 	assert.Equal(t, int64(30), u.ID)
 	assert.NoError(t, err)
+}
+
+// Note: more thorough testing of encode/decode stuff requires us to mock out the
+// time package, which would be a big PITA
+func TestDecodeUser(t *testing.T) {
+	token := encodeUser(&user.User{
+		ID:     30,
+		Status: user.StatusActive,
+	})
+	assert.NotEmpty(t, token)
+
+	du, err := decodeUser(token)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(30), du.ID)
+	assert.Equal(t, user.Status(1), du.Status)
+
+	// test old token
+	_, err = decodeUser(expiredToken)
+	assert.Equal(t, errors.ExpiredToken, err)
+
+	// test garbage token
+	_, err = decodeUser("garbage_token")
+	assert.Equal(t, errors.InvalidToken, err)
 }
