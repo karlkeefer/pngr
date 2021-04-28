891b587b6de5cc1fffcd85443d3b9605ab5eb2c4
Karl Keefer <karl@karlkeefer.com>
2021-04-28 09:54:41 -0700

    bump golang version and de-register serviceworker by default

--- react/src/index.js
+++ react/src/index.js
@@ -5,7 +5,18 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// if you want to run this as a progressive webapp, you can register this service worker, 
+// but you also need to add handling for upgrades otherwise folks will get stuck with old versions of your app indefinitely
+// registerServiceWorker();
+
+// unregister all serviceWorkers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
