9f6475cd2874c148da7d5061a4f00970848d776b
Karl Keefer <karl@karlkeefer.com>
2021-08-26 15:46:20 -0700

    improved cors and csrf middlewares, plus lag-by-default in dev environment

--- golang/server/server.go
+++ golang/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/karlkeefer/pngr/golang/env"
 	"github.com/karlkeefer/pngr/golang/errors"
@@ -13,6 +14,16 @@ import (
 	"github.com/karlkeefer/pngr/golang/server/write"
 	"github.com/karlkeefer/pngr/golang/utils"
 )
+
+var isDev = false
+var appRoot = ""
+
+const localDev = "https://localhost"
+
+func init() {
+	isDev = os.Getenv("ENV") == "dev"
+	appRoot = os.Getenv("APP_ROOT")
+}
 
 type server struct {
 	env env.Env
@@ -33,9 +44,10 @@ func New() (*server, error) {
 
 // ServeHTTP forks API traffic from static asset traffic
 func (srv *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	originalPath := r.URL.Path
 	handler := srv.getHandler(w, r)
 	// TODO: consider a middleware wrapper utility
-	wrappedHandler := lag(csrf(cors(handler)))
+	wrappedHandler := lag(csrf(originalPath, cors(originalPath, handler)))
 	wrappedHandler(w, r)
 }
 
@@ -76,33 +88,81 @@ func (srv *server) getHandler(w http.ResponseWriter, r *http.Request) http.Handl
 // MIDDLEWARE
 
 // lag allows you to simiulate API lag locally to test "loading" states
-// just uncomment the time.Sleep to see it in action
 func lag(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// time.Sleep(time.Millisecond * 1000)
+		if isDev {
+			// minimal lag so that we can see loading states at least briefly
+			time.Sleep(time.Millisecond * 500)
+		}
 		fn(w, r)
 	}
 }
 
+// only returns an origin if it matches our list
+func validateOrigin(r *http.Request) string {
+	switch r.Header.Get("Origin") {
+	case appRoot:
+		return appRoot
+	case localDev:
+		return localDev
+	default:
+		return ""
+	}
+}
+
 // csrf checks for the CSRF prevention header and compares the origin header
-func csrf(fn http.HandlerFunc) http.HandlerFunc {
+func csrf(path string, fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Origin") != "" && r.Header.Get("Origin") != os.Getenv("APP_ROOT") {
-			fn = write.Error(errors.BadOrigin)
-		} else if r.Header.Get("X-Requested-With") != "XMLHttpRequest" {
-			fn = write.Error(errors.BadCSRF)
+		if skipCorsAndCSRF(path) {
+			fn(w, r)
+			return
+		}
+
+		if r.Method != http.MethodOptions {
+			if r.Header.Get("Origin") != "" && validateOrigin(r) == "" {
+				// if an origin is provided, but didn't match our list
+				fn = write.Error(errors.BadOrigin)
+			} else if r.Header.Get("X-Requested-With") != "XMLHttpRequest" {
+				fn = write.Error(errors.BadCSRF)
+			}
 		}
 		fn(w, r)
 	}
 }
 
 // cors adds CORS headers to the response
-// the current setup does not allow any cross-origin requests!
-func cors(fn http.HandlerFunc) http.HandlerFunc {
+func cors(path string, fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("APP_ROOT"))
+		if skipCorsAndCSRF(path) {
+			fn(w, r)
+			return
+		}
+
+		if origin := validateOrigin(r); origin != "" {
+			// if we were given an origin that matches our list
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With")
+
+		if r.Method == http.MethodOptions {
+			// simple response for the preflight check
+			fn = write.Success()
+		}
 		fn(w, r)
 	}
+}
+
+// a list of paths to bypass cors checks - this is useful for webhooks and stuff
+var bypassPaths = []string{}
+
+func skipCorsAndCSRF(path string) bool {
+	for _, c := range bypassPaths {
+		if path == c {
+			return true
+		}
+	}
+
+	return false
 }
