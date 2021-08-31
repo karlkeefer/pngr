87153de630b8ca38eb5999a9318fc8f834df0a31
Karl Keefer <karl@karlkeefer.com>
2021-08-31 14:25:44 -0700

    fix hotreload

--- react/src/Routes/Home/Home.js
+++ react/src/Routes/Home/Home.js
@@ -1,21 +1,18 @@
 import React from 'react'
-import { Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import SimplePage from 'Shared/SimplePage';
 
 const Home = () => (
   <SimplePage icon='rocket' title='Welcome to PNGR!'>
-    <Segment>
-      <p>This is a boilerplate app using React for the front-end, and Golang + Postgres for the backend.</p>
-      <p>The only things implemented are...</p>
-      <ul>
-        <li>Account Creation</li>
-        <li>Session Management</li>
-        <li><b>CR</b>eate<b>U</b>pdate<b>D</b>elete for simple "posts"</li>
-      </ul>
-      <p><Link to="/signup">Sign Up</Link> to see how sessions work and create/view secured posts.</p>
-    </Segment>
+    <p>This is a boilerplate app using React for the front-end, and Golang + Postgres for the backend.</p>
+    <p>The only things implemented are...</p>
+    <ul>
+      <li>Account Creation</li>
+      <li>Session Management</li>
+      <li><b>CRUD</b> for simple "posts" content type.</li>
+    </ul>
+    <p><Link to="/signup">Sign Up</Link> to see how sessions work and create/view secured posts.</p>
   </SimplePage>
 )
 
