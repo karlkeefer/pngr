38c076b1b405d87137ac71575baf6ecd5bdff756
Karl Keefer <karl@karlkeefer.com>
2021-09-01 17:39:51 -0700

    fix login to use salt from DB

--- golang/handlers/session.go
+++ golang/handlers/session.go
@@ -11,20 +11,25 @@ import (
 	"github.com/karlkeefer/pngr/golang/handlers/write"
 )
 
+type loginRequest struct {
+	Email string
+	Pass  string
+}
+
 func Login(env env.Env, user *db.User, w http.ResponseWriter, r *http.Request) http.HandlerFunc {
 	decoder := json.NewDecoder(r.Body)
-	u := db.User{}
-	err := decoder.Decode(&u)
-	if err != nil || &u == nil {
+	req := loginRequest{}
+	err := decoder.Decode(&req)
+	if err != nil || &req == nil {
 		return write.Error(errors.NoJSONBody)
 	}
 
-	fromDB, err := env.DB().FindUserByEmail(r.Context(), u.Email)
+	u, err := env.DB().FindUserByEmail(r.Context(), req.Email)
 	if err != nil {
 		return write.Error(err)
 	}
 
-	if !checkPasswordHash(u.Pass, u.Salt, fromDB.Pass) {
+	if !checkPasswordHash(req.Pass, u.Salt, u.Pass) {
 		return write.Error(errors.FailedLogin)
 	}
 
