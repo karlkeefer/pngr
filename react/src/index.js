56173817bf990f833746afbb9428ec6ca9485dcc
Karl Keefer <karl@karlkeefer.com>
2021-09-02 09:58:17 -0700

    configurable semantic-ui themes

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-less/semantic.less'
 import './index.css';
 
 import App from 'App';
