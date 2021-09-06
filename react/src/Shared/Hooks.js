66395224783a90fd28195d8e1fa8f9cc25a60df9
Karl Keefer <karl@karlkeefer.com>
2021-09-06 13:52:56 -0700

    small improvements to shared/hooks

--- react/src/Shared/Hooks.js
+++ react/src/Shared/Hooks.js
@@ -1,23 +1,27 @@
 import { useState, useCallback } from 'react';
+import _ from 'lodash'
 
 export const useRequest = (initData) => {
   const [data, setData] = useState(initData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const run = useCallback((promise, onSuccess) => {
+  const run = useCallback((promise, onSuccess, onFailure) => {
     setLoading(true);
     setError('');
 
     return promise
       .then(data => {
-        setData(data);
-        setLoading(false);
         if (onSuccess) {
           onSuccess(data);
         }
+        setData(data);
+        setLoading(false);
       })
       .catch(error => {
+        if (onFailure) {
+          onFailure(error);
+        }
         setError(error);
         setLoading(false);
       });
@@ -26,14 +30,19 @@ export const useRequest = (initData) => {
   return [loading, error, run, data];
 }
 
+// useFields gives us a simple handleChange that works for most form inputs.
+// this hook also supports nested properties!
+// You just have to set your input field's name attr appropriately
+// e.g. w/ a schema like {person:{first_name:''}} you can do <input name="person.first_name"/>
 export const useFields = (initFields) => {
   const [fields, setFields] = useState(initFields)
   const handleChange = useCallback((e, {name, type, value, checked}) => {
-      setFields(f => {
-        f = {...f, [name]: type === 'checkbox' ? checked : value };
-        return f
-      });
-    }, [setFields])
+    setFields(f => {
+      let out = _.cloneDeep(f)
+      _.set(out, name, type === 'checkbox' ? checked : value);
+      return out
+    });
+  }, [setFields])
 
   return [fields, handleChange, setFields];
 }
