7a38a59eed77c3d82b8949249010bc86b1d6e6db
Karl Keefer <karl@karlkeefer.com>
2023-02-21 23:55:40 -0800

    fix deps, improve unexpected error logging

--- react/src/registerServiceWorker.tsx
+++ react/src/registerServiceWorker.tsx
@@ -77,7 +77,7 @@ function registerValidSW(swUrl: string) {
       };
     })
     .catch(error => {
-      console.error('Error during service worker registration:', error);
+      console.Error(r.Context(), 'Error during service worker registration:', error);
     });
 }
 
